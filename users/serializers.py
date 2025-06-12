8a20f70c193c2e10778a9bc498cc155b1989d8aa
ngengchhengkim-0714 <ngeng.chhengkim@sun-asterisk.com>
2025-06-12 12:11:17 +0700

    [Day 3] Get/post article comment and get profile user

--- users/serializers.py
+++ users/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 from django.conf import settings
 from .models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['username', 'bio', 'image']
+
+class ProfileSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ['username', 'bio', 'image']
