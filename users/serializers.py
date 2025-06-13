b2553c6c163f66931b8d4b383ae3a92a82498357
ngengchhengkim-0714 <ngeng.chhengkim@sun-asterisk.com>
2025-06-13 15:26:09 +0700

    [Day-4] User registration

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.conf import settings
 from .models import User
+from rest_framework.validators import UniqueValidator
+
 
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
@@ -11,3 +13,14 @@ class ProfileSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ['username', 'bio', 'image']
+
+class RegisterSerializer(serializers.ModelSerializer):
+  email = serializers.EmailField(
+    required=True,
+    validators=[UniqueValidator(queryset=User.objects.all())]
+  )
+  password = serializers.CharField(write_only=True, min_length=8, max_length=32)
+
+  class Meta:
+    model = User
+    fields = ['email', 'username', 'bio', 'image', 'password']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from users.serializers import ProfileSerializer
+from users.serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -8,3 +12,22 @@ class ProfileView(generics.RetrieveAPIView):
   queryset = User.objects.all()
   serializer_class = ProfileSerializer
   lookup_field = 'username'
+
+class RegisterView(APIView):
+  """
+  View to handle user registration.
+  """
+  def post(self, request):
+    serializer = RegisterSerializer(data=request.data['user'])
+    if serializer.is_valid():
+      user = serializer.save()
+      refresh = RefreshToken.for_user(user)
+      result = serializer.data
+      result = {
+        'success': True,
+        'user': serializer.data
+      }
+      result['user']['token'] = str(refresh.access_token)
+
+      return Response(result, status=201)
+    return Response(serializer.errors, status=400)
